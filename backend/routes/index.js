b5515bb4af21fac99e01df4e281b11c5671d3c80
micLira828 <micgep1015@gmail.com>
2024-04-25 12:44:49 -0700

    Initialize Express and Sequelize with CSRF protection

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,15 +1,13 @@
 // backend/routes/index.js
 const express = require('express');
 const router = express.Router();
-const apiRouter = require('./api');
 
 router.get('/hello/world', function(req, res) {
   res.cookie('XSRF-TOKEN', req.csrfToken());
   res.send('Hello World!');
 });
 
-// backend/routes/index.js
-// ...
+
 // Add a XSRF-TOKEN cookie
 router.get("/api/csrf/restore", (req, res) => {
   const csrfToken = req.csrfToken();
@@ -18,11 +16,6 @@ router.get("/api/csrf/restore", (req, res) => {
     'XSRF-Token': csrfToken
   });
 });
-// ...
-
-
-
-router.use('/api', apiRouter);
 // ...
 
 module.exports = router;

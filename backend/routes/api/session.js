7329871a02584b05f49415fc9a2ef321fc98f22e
micLira828 <micgep1015@gmail.com>
2024-04-26 12:57:40 -0700

    re-added a backend endpoint to get the current user session

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -51,6 +51,29 @@ router.post(
     });
   }
 );
+
+// backend/routes/api/session.js
+// ...
+
+// Restore session user
+router.get(
+  '/',
+  (req, res) => {
+    const { user } = req;
+    if (user) {
+      const safeUser = {
+        id: user.id,
+        email: user.email,
+        username: user.username,
+      };
+      return res.json({
+        user: safeUser
+      });
+    } else return res.json({ user: null });
+  }
+);
+
+// ...
 // backend/routes/api/session.js
 // ...
 // backend/routes/api/session.js

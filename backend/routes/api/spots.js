8d93dd7084df0747c70e7c0d2640bc07f5ce31bc
micLira828 <micgep1015@gmail.com>
2024-04-30 20:59:38 -0700

    I successfully added route handlers to update and delete a post

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -19,11 +19,12 @@ router.get('/:spotId', async(req, res) => {
 });
 
 
-router.post('api/spots', async(req, res) => {
+router.post('/', async(req, res) => {
      const spot = await Spot.create(
         { 
          latitude: req.body.latitude, 
          longitude: req.body.longitude,
+         ownerId: req.body.ownerId,
          address: req.body.address,
          name: req.body.name,
          country: req.body.country,
@@ -33,5 +34,42 @@ router.post('api/spots', async(req, res) => {
          price: req.body.price
         }
     );
+
+    res.json(spot);
  });
+
+ router.put('/:spotId', async(req, res) => {
+   const spot_id = req.params.spotId;
+   const spot= await Spot.findByPk(spot_id);
+   await spot.update(
+      { 
+       latitude: req.body.latitude, 
+       longitude: req.body.longitude,
+       ownerId: req.body.ownerId,
+       address: req.body.address,
+       name: req.body.name,
+       country: req.body.country,
+       city: req.body.city,
+       state: req.body.state,
+       description: req.body.description,
+       price: req.body.price
+      }
+  );
+
+  res.json(spot);
+});
+
+
+router.delete('/:spotId', async(req, res) => {
+   const spot_id = req.params.spotId;
+   const spot= await Spot.findByPk(spot_id);
+   
+   spot.destroy();
+   res.json({
+      "message": "Successfully deleted"
+    });
+});
+
+
+
 module.exports = router;

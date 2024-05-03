3736788d2f417de0f9276e08c2c8b8b687faf864
micLira828 <micgep1015@gmail.com>
2024-05-02 21:52:07 -0700

    I passed the options parameter in the up functions on my migrations

--- backend/db/migrations/20240423044308-create-spot-image.js
+++ backend/db/migrations/20240423044308-create-spot-image.js
@@ -28,7 +28,7 @@ module.exports = {
         allowNull: false,
         type: Sequelize.DATE
       }
-    });
+    }, options);
   },
   async down(queryInterface, Sequelize) {
     options.tableName = "SpotImages";
--- backend/db/migrations/20240423044312-create-review.js
+++ backend/db/migrations/20240423044312-create-review.js
@@ -43,7 +43,7 @@ module.exports = {
         allowNull: false,
         type: Sequelize.DATE
       }
-    });
+    }, options);
   },
   async down(queryInterface, Sequelize) {
     options.tableName = "Reviews";

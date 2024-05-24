2059474a93fcf152bb900e474f0778b60a00bda4
micLira828 <micgep1015@gmail.com>
2024-05-23 19:36:16 -0700

    Fixed my response to get the current user

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -77,8 +77,10 @@ router.get(
     if (user) {
       const safeUser = {
         id: user.id,
+        firstName: user.firstName,
+        lastName: user.lastName,
         email: user.email,
-        username: user.username,
+        username: user.username
       };
       return res.json({
         user: safeUser

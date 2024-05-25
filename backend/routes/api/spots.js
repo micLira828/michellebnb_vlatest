58f8d29bf420178dd670801fa4c9486bf3ef964b
micLira828 <micgep1015@gmail.com>
2024-05-25 16:47:44 -0700

    I fixed the bad request response for creating and editing a spot

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -3,14 +3,101 @@ const express = require('express');
 const { Spot, Review, Booking, SpotImage, ReviewImage, User } = require('../../db/models');
 const bcrypt = require('bcryptjs');
 const { check } = require('express-validator');
-const { handleValidationErrors } = require('../../utils/validation');
+const {handleValidationErrors } = require('../../utils/validation');
 const { setTokenCookie, requireAuth } = require('../../utils/auth');
 const { parseTwoDigitYear } = require('moment');
-const { Op, Sequelize, where} = require('sequelize');
+const { Op, Sequelize, where, ValidationError} = require('sequelize');
 
 
 const router = express.Router();
 var today = new Date();
+
+   const validateSpot= [
+   check('address')
+     .exists({ checkFalsy: true })
+     .isString()
+     .notEmpty()
+     .withMessage('Street address is required'),
+    check('city')
+     .exists({ checkFalsy: true })
+     .isString()
+     .notEmpty()
+     .withMessage('City is required'),
+    check('state')
+     .exists({ checkFalsy: true })
+     .isString()
+     .notEmpty()
+     .withMessage('State is required'),
+    check('country')
+     .exists({ checkFalsy: true })
+     .isLength({ min: 6 })
+     .withMessage('Country is required.'),
+     check('lat')
+     .exists({ checkFalsy: true })
+     .isFloat({ min: -90, max: 90 })
+     .withMessage('Latitude must be within -90 and 90'),
+     check('lng')
+     .exists({ checkFalsy: true })
+     .isFloat({ min: -180, max: 180 })
+     .withMessage('Longitude must be within -180 and 180'),
+      check('name')
+     .exists({ checkFalsy: true })
+     .isLength({ max: 50 })
+     .withMessage('Name must be less than 50 characters'),
+      check('description')
+     .exists({ checkFalsy: true })
+     .withMessage('Description is Required'),
+     check('price')
+     .isFloat({ min: 0, max: 2000})
+     .withMessage('Price per day must be a positive number'),
+   handleValidationErrors
+ ];
+
+// const checkIfInRange = (req, res, next) => {
+//    const errors = [];
+  
+//   /**
+
+//    */
+//    try {
+//       // console.log(req);
+//       const {address, lat, lng, price} = req.body;
+//       console.log(`${lat}, ${lng}, ${price}`)
+//       const errors = [];
+//       if(!address || address.length === 0){
+//          errors.push({address: 'Street address is required'})
+//       }
+//       if(lat < -90 || lat > 90){
+//          errors.push({Latitude: 'Latitude must be within -90 and 90'})
+//       }
+//       if(errors.length > 0){
+//          const customError = new ValidationError();
+//          customError.errors = errors;
+//          throw errors;
+//       }
+//       next();
+//    } catch (error) {
+//       const customError = new ValidationError();
+//       customError.errors = error;
+//       //error.status = 400;
+//       next(customError);
+//    }
+// }
+
+// const checkIfInRange = (val, minVal, maxVal, msg) => {
+//    if(val < minVal || val > maxVal){
+//       err.message = msg
+//    }
+// }
+
+router.use((err, req, res, next) => {
+   console.error(err);
+   res.status(err.status || 500);
+   res.send('An error occurred! Please check the url, or wait a few minutes and try again.');
+   next(err);
+ });
+
+ 
 
 const validateReview = [
    check('review')
@@ -38,47 +125,7 @@ const validateReview = [
  ];
 
 
-const validateSpot= [
-   check('address')
-     .exists({ checkFalsy: true })
-     .isString()
-     .notEmpty()
-     .withMessage('Street address is required'),
-    check('city')
-     .exists({ checkFalsy: true })
-     .isString()
-     .notEmpty()
-     .withMessage('City is required'),
-    check('state')
-     .exists({ checkFalsy: true })
-     .isString()
-     .notEmpty()
-     .withMessage('State is required'),
-    check('country')
-     .exists({ checkFalsy: true })
-     .isLength({ min: 6 })
-     .withMessage('Country is required.'),
-     check('lat')
-     .exists({ checkFalsy: true })
-     .isDecimal({min:-90.0, max:90.0})
-     .withMessage('Latitude must be within -90 and 90'),
-     check('lng')
-     .exists({ checkFalsy: true })
-     .isDecimal({min:-180.0, max: 180.0})
-     .withMessage('Longitude must be within -180 and 180'),
-      check('name')
-     .exists({ checkFalsy: true })
-     .isLength({ max: 50 })
-     .withMessage('Name must be less than 50 characters'),
-      check('description')
-     .exists({ checkFalsy: true })
-     .withMessage('Description is Required'),
-      check('price')
-     .exists({ checkFalsy: true })
-     .isDecimal({min:1.0, max:2000.0})
-     .withMessage('Price per day must be a positive number'),
-   handleValidationErrors
- ];
+
 //Gets all of the spots
 router.get('/', async(req, res) => {
    let {page, size, minLat, maxLat, minLong, maxLong, minPrice, maxPrice} = req.query
@@ -325,7 +372,6 @@ router.post('/', requireAuth, validateSpot, async(req, res) => {
       }
   );
 
-  
     res.status(201).json(spot);
  });
 
--- backend/utils/validation.js
+++ backend/utils/validation.js
@@ -1,17 +1,23 @@
 // backend/utils/validation.js
-const { validationResult } = require('express-validator');
+const { validationResult} = require('express-validator');
 
 // middleware for formatting errors from express-validator middleware
 // (to customize, see express-validator's documentation)
 const handleValidationErrors = (req, _res, next) => {
   const validationErrors = validationResult(req);
+  console.log(req);
 
   if (!validationErrors.isEmpty()) { 
     const errors = {};
     validationErrors
       .array()
       .forEach(error => errors[error.path] = error.msg);
-
+      const {lat, lng, price} = req.body;
+      // console.log(`${lat}, ${lng}, ${price}`)
+      // if(lat < -90 || lat > 90){
+      //    throw new Error('Latitude must be within -90 and 90')
+      // }
+    console.log(validationErrors);
     const err = Error("Bad request.");
     err.errors = errors;
     err.status = 400;
@@ -20,6 +26,7 @@ const handleValidationErrors = (req, _res, next) => {
   }
   next();
 };
+
 
 module.exports = {
   handleValidationErrors

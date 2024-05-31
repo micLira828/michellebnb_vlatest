fcb0b65e208dfda1c7c176870646dde8bae1d6b9
micLira828 <micgep1015@gmail.com>
2024-05-30 17:55:20 -0700

    Fixed a status code of a created image

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -382,7 +382,7 @@ router.post('/', requireAuth, validateSpot, async(req, res) => {
          spotId: spotId
       });
 
-      res.json({"id":spotImage.id, "url":spotImage.url, "preview": spotImage.preview});
+      res.status(201).json({"id":spotImage.id, "url":spotImage.url, "preview": spotImage.preview});
    }
 
    else{

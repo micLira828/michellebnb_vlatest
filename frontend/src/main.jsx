94c4da3e3ad92dd3c86377d9f08e569668d937e0
micLira828 <micgep1015@gmail.com>
2024-06-25 21:44:37 -0700

    I added a frontend to the application and tested it and added files for the csrf

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,6 +6,9 @@ import './index.css';
 import configureStore from './store';
 
 const store = configureStore();
+if (process.env.NODE_ENV !== 'production') {
+  window.store = store;
+}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>

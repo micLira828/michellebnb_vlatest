fcd625b62868969625f75873cd77008d15ee49b8
micLira828 <micgep1015@gmail.com>
2024-07-02 16:16:35 -0700

    Styled spots cards

--- frontend/src/components/Spots/Card.jsx
+++ frontend/src/components/Spots/Card.jsx
@@ -1,13 +1,13 @@
+import './Card.css'
 const Card = ({spot}) => {
     console.log('The spot is', spot);
     return (
-        <>
-          <h2>{spot.name}</h2>
-          <p>{spot.city}</p>
-          <p>{spot.state}</p>
-          <p>${spot.price} night</p>
-          <p>{spot.avgRating ? spot.avgRating: "New"}</p>
-        </>
+        <div className = 'card'>
+          <h3>{spot.name}</h3>
+          <p><em>{spot.city}, {spot.state}</em> <br></br>
+          <strong>${spot.price} night </strong><br></br>
+          {spot.avgRating ? spot.avgRating: "New"}</p>
+        </div>
     );
 }
 
--- frontend/src/components/Spots/Spots.jsx
+++ frontend/src/components/Spots/Spots.jsx
@@ -2,6 +2,7 @@ import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { getAllSpots } from '../../store/spot';
 import Card from './Card'
+import './Spots.css'
 
 const Spots = () => {
     const dispatch = useDispatch();
@@ -15,11 +16,11 @@ const Spots = () => {
       }, [dispatch]);
 
     return (
-        <>
+        <div className = "spots">
              {spotsList?.map((spot) => (
                  <Card key = {spot.id} spot={spot}/>
             ))}
-        </>
+        </div>
     );
 }
 

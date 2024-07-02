632a6781cb94321c5d9d4718fd4798e74b34762f
micLira828 <micgep1015@gmail.com>
2024-07-02 12:54:50 -0700

    Got everything up to date in project

--- frontend/src/components/SignupFormModal/SignupFormModal.jsx
+++ frontend/src/components/SignupFormModal/SignupFormModal.jsx
@@ -1,6 +1,6 @@
 // frontend/src/components/SignupFormPage/SignupFormPage.jsx
 
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Navigate } from 'react-router-dom';
 import * as sessionActions from '../../store/session';
@@ -17,6 +17,8 @@ function SignupFormPage() {
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
   const [errors, setErrors] = useState({});
+  const [buttonOut, setButtonOut] = useState(true);
+
 
   if (sessionUser) return <Navigate to="/" replace={true} />;
 
@@ -39,11 +41,25 @@ function SignupFormPage() {
         }
       });
     }
+
     return setErrors({
+      username: "Username should be 6 or more characters",
+      password: "Password should be 4 or more characters",
       confirmPassword: "Confirm Password field must be the same as the Password field"
     });
   };
 
+  useEffect(() => {
+    setErrors({});
+
+    if(email.length >= 4 
+      && username.length >= 4 &&
+      password.length >= 6 &&
+      confirmPassword === password){
+        setButtonOut(false);
+    }
+  }, [email, username, firstName, lastName, password, confirmPassword])
+  
   return (
     <>
       <h1>Sign Up</h1>
@@ -57,7 +73,7 @@ function SignupFormPage() {
             required
           />
         </label>
-        {errors.email && <p>{errors.email}</p>}
+        {errors.email && <p className="errors">{errors.email}</p>}
         <label>
           Username
           <input
@@ -67,7 +83,7 @@ function SignupFormPage() {
             required
           />
         </label>
-        {errors.username && <p>{errors.username}</p>}
+        {errors.username && <p className="errors">{errors.username}</p>}
         <label>
           First Name
           <input
@@ -77,7 +93,7 @@ function SignupFormPage() {
             required
           />
         </label>
-        {errors.firstName && <p>{errors.firstName}</p>}
+        {errors.firstName && <p className="errors">{errors.firstName}</p>}
         <label>
           Last Name
           <input
@@ -87,7 +103,7 @@ function SignupFormPage() {
             required
           />
         </label>
-        {errors.lastName && <p>{errors.lastName}</p>}
+        {errors.lastName && <p className="errors">{errors.lastName}</p>}
         <label>
           Password
           <input
@@ -97,7 +113,7 @@ function SignupFormPage() {
             required
           />
         </label>
-        {errors.password && <p>{errors.password}</p>}
+        {errors.password && <p className="errors">{errors.password}</p>}
         <label>
           Confirm Password
           <input
@@ -107,8 +123,8 @@ function SignupFormPage() {
             required
           />
         </label>
-        {errors.confirmPassword && <p>{errors.confirmPassword}</p>}
-        <button type="submit">Sign Up</button>
+        {errors.confirmPassword && <p className="errors">{errors.confirmPassword}</p>}
+        <button disabled = {buttonOut ? true: false} type="submit">Sign Up</button>
       </form>
     </>
   );

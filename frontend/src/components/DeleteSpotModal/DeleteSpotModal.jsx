5fe9e6534c52a939f4c14862e8bdd234cc45cbd2
micLira828 <micgep1015@gmail.com>
2024-07-12 11:52:42 -0700

    Changed styling of delete button

--- frontend/src/components/DeleteSpotModal/DeleteSpotModal.jsx
+++ frontend/src/components/DeleteSpotModal/DeleteSpotModal.jsx
@@ -2,6 +2,7 @@ import {useState} from 'react';
 import {useDispatch, useSelector} from 'react-redux'
 import {removeSpot} from '../../store/spot';
 import { useModal } from '../../context/Modal';
+import './DeleteModal.css'
 
 const DeleteSpotModal = ({spotId}) =>{
     let spot = useSelector((state) => state.spots.byId[spotId]);
@@ -14,12 +15,12 @@ const DeleteSpotModal = ({spotId}) =>{
         dispatch(removeSpot(spot)).then(closeModal)
     }
     
-    return (<>
+    return (<div className = "deleteModalContainer">
         <p>Are you sure you want to
         remove the spot from the listings?</p>
-        <button className = "redRectangular" onClick = {deleteTheSpot}>Yes(Delete Spot)</button>
-        <button onClick = {closeModal}>No(Keep Spot)</button>
-    </>)
+        <button className = "redRectangular deleteButton" onClick = {deleteTheSpot}>Yes(Delete Spot)</button>
+        <button className = "deleteButton" onClick = {closeModal}>No(Keep Spot)</button>
+    </div>)
     
 }
 

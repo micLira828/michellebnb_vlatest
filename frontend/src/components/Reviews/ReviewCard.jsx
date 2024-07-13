e49581e6c257be0f642947c56b0761f4e2d5bdbb
micLira828 <micgep1015@gmail.com>
2024-07-13 00:25:00 -0700

    I added the first name and month and year to the review text and put it in columns

--- frontend/src/components/Reviews/ReviewCard.jsx
+++ frontend/src/components/Reviews/ReviewCard.jsx
@@ -1,11 +1,27 @@
 import { NavLink } from "react-router-dom"
+import moment from 'moment'
 const ReviewCard = ({review}) => {
 
-    return (<>
+    // const createdAtReviewArr = review.createdAt.split('');
+    // const date = createdAtReviewArr[0];
+    // const dateArr = date.split('-');
+    // const year = dateArr[0];
+    // const twoDigitMonth = dateArr[1].toString();
+    // const monthDigit = twoDigitMonth[1];
+
+    // const monthsArray = ['January', 'February', 'March', 'April',
+    //      'May', 'June', 'July', 'August', 'September', 'October', 'November', '']
+
+    const verbalDate = moment(review.createdAt).format("MMMM YYYY"); // "Sunday, February 14th 2010, 3:25:50 pm"
+    
+    
+    return (<div className = "review">
+    <h4>{review.User.firstName}</h4>
+    <h4>{verbalDate}</h4>
      <p>{review.review}</p>
      <NavLink to = {`/reviews/${review.id}/edit`}><button>Edit a Review</button></NavLink>
      <NavLink to = {`/reviews/${review.id}/delete`}><button>Delete a Review</button></NavLink>
-    </>)
+    </div>)
    
 }
 
--- frontend/src/components/Reviews/Reviews.jsx
+++ frontend/src/components/Reviews/Reviews.jsx
@@ -3,6 +3,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import ReviewCard from './ReviewCard';
 import { getSpotReviews } from '../../store/review';
 import CreateReview from './CreateReview';
+import './Reviews.css';
 const Reviews = (spot) => {
   const dispatch = useDispatch();
 
@@ -14,9 +15,9 @@ const Reviews = (spot) => {
   return (
     <>
     {
-      <ul>
+      <div className = "reviews">
         {spotReviews.map((review) => <ReviewCard review = {review} key = {review.id}/>)}
-      </ul>
+      </div>
     }
    <button>Post a Review</button>
    <CreateReview spot = {spot}/>
